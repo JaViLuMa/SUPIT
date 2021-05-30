758e61c5dafe3df80c7d170f499deb901abfe887
JaViLuMa <matej.gamer01@gmail.com>
2021-05-30 22:35:27 +0200

    Update prettier config

--- prettier-config.js
+++ prettier-config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    tabWidth: 2,
-    useTabs: false,
-    singleQuote: true,
-    trailingComma: 'es5',
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    arrowParens: 'always',
-    printWidth: 80,
-    endOfLine: 'auto',
-    overrides: [
-      {
-        files: ['*.scss', '*.css'],
-        options: {
-          singleQuote: false,
-        },
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  printWidth: 80,
+  endOfLine: "auto",
+  overrides: [
+    {
+      files: ["*.css"],
+      options: {
+        singleQuote: false,
       },
-    ],
-  };
+    },
+  ],
+};
